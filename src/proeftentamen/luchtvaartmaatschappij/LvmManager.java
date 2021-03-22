303fd8c40e823f26118d0b50bf43ff9fe96b3da8
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2021-03-22 10:47:49 +0100

    Updated answers

--- src/proeftentamen/luchtvaartmaatschappij/LvmManager.java
+++ src/proeftentamen/luchtvaartmaatschappij/LvmManager.java
@@ -2,12 +2,15 @@ package proeftentamen.luchtvaartmaatschappij;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class LvmManager {
 	public LuchtvaartMaatschappij lvmInladen(String naam) throws IOException, ClassNotFoundException {
-		FileInputStream fis = new FileInputStream(naam);
-		ObjectInputStream ois = new ObjectInputStream(fis);
+		InputStream is = Files.newInputStream(Path.of(naam));
+		ObjectInputStream ois = new ObjectInputStream(is);
 
 		LuchtvaartMaatschappij deLvm = (LuchtvaartMaatschappij) ois.readObject();
 		ois.close();
